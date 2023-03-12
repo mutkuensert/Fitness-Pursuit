a017f1316f37c733107da76800c3b80f5b39e06e
M. Utku Ensert <mutkuensert@hotmail.com>
2023-03-12 18:16:12 +0300

    Feature/Body sizes: - Add date and time picker on BodySizes screen.

--- app/src/main/java/com/mutkuensert/fitnesspursuit/features/bodysizes/BodySizes.kt
+++ app/src/main/java/com/mutkuensert/fitnesspursuit/features/bodysizes/BodySizes.kt
@@ -1,5 +1,7 @@
 package com.mutkuensert.fitnesspursuit.features.bodysizes
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -7,17 +9,22 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mutkuensert.fitnesspursuit.R
@@ -25,12 +32,15 @@ import com.mutkuensert.fitnesspursuit.core.getStringRes
 import com.mutkuensert.fitnesspursuit.resources.TextResKeys
 import com.mutkuensert.fitnesspursuit.ui.theme.TextColors
 import com.mutkuensert.fitnesspursuit.ui.theme.appTypography
+import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalTime
 
 private const val VERTICAL_SPACE = 5
 
 @Composable
 fun BodySizes() {
+    val context = LocalContext.current
     val (leftBicep, onLeftBicepChange) = remember { mutableStateOf("") }
     val (rightBicep, onRightBicepChange) = remember { mutableStateOf("") }
     val (leftForearm, onLeftForearmChange) = remember { mutableStateOf("") }
@@ -44,7 +54,33 @@ fun BodySizes() {
     val (neck, onNeckChange) = remember { mutableStateOf("") }
     val (shoulders, onShouldersChange) = remember { mutableStateOf("") }
     val (waist, onWaistChange) = remember { mutableStateOf("") }
-    val time = LocalDateTime.now()
+    var dateTime by remember { mutableStateOf(LocalDateTime.now()) }
+
+    val timePicker =
+        TimePickerDialog(
+            context,
+            TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+                dateTime = LocalDateTime.of(dateTime.toLocalDate(), LocalTime.of(hourOfDay, minute))
+            },
+            dateTime.hour,
+            dateTime.minute,
+            true
+        )
+
+    val datePicker = DatePickerDialog(
+        context,
+        DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            val time = dateTime.toLocalTime()
+            dateTime = LocalDateTime.of(
+                LocalDate.of(year, month, dayOfMonth),
+                LocalTime.of(time.hour, time.minute)
+            )
+            timePicker.show()
+        },
+        dateTime.year,
+        dateTime.monthValue,
+        dateTime.dayOfMonth
+    )
 
     Column(
         modifier = Modifier
@@ -123,11 +159,23 @@ fun BodySizes() {
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        with(time) {
-            Text(
-                text = "$dayOfMonth.$monthValue.$year $hour:$minute",
-                style = MaterialTheme.appTypography.h6,
-                color = TextColors.viridianGreen
+        Row(
+            modifier = Modifier.clickable { datePicker.show() },
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            with(dateTime) {
+                Text(
+                    text = "$dayOfMonth.$monthValue.$year $hour:$minute ",
+                    style = MaterialTheme.appTypography.h6,
+                    color = TextColors.viridianGreen
+                )
+            }
+
+            Icon(
+                modifier = Modifier.requiredHeight(20.dp),
+                painter = painterResource(R.drawable.open),
+                tint = TextColors.viridianGreen,
+                contentDescription = getStringRes(TextResKeys.SET_CUSTOM_DATE_TIME)
             )
         }
 
--- app/src/main/java/com/mutkuensert/fitnesspursuit/resources/TextResKeys.kt
+++ app/src/main/java/com/mutkuensert/fitnesspursuit/resources/TextResKeys.kt
@@ -28,4 +28,5 @@ object TextResKeys {
     const val WARMUP_SETS = "warmup_sets"
     const val ONE_RM = "one_rm"
     const val BODY_SIZES = "body_sizes"
+    const val SET_CUSTOM_DATE_TIME = "set_custom_date_time"
 }
