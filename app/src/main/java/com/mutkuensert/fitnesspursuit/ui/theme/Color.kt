33189191ab6e7abb806c56db37527b751130b8b4
M. Utku Ensert <mutkuensert@hotmail.com>
2023-06-14 01:47:58 +0300

    Remove unused colors.

--- app/src/main/java/com/mutkuensert/fitnesspursuit/ui/theme/Color.kt
+++ app/src/main/java/com/mutkuensert/fitnesspursuit/ui/theme/Color.kt
@@ -10,8 +10,5 @@ val Teal200 = Color(0xFF03DAC5)
 object AppColors {
     val primaryDark = Color(0xFF000000)
     val persianGreen = Color(0xFF2A9D8F)
-    val charcoalGreen = Color(0xFF264653)
-    val saffron = Color(0xFFE9C46A)
-    val sandyBrown = Color(0xFFF4A261)
     val burntSienna = Color(0xFFE76F51)
 }
